391beba45e9c00ea73aacc5252e1010c735e174a
Jan Kuthan <jan.kuthan@lmc.eu>
2020-11-24 15:58:52 +0100

    Examples

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $posts = $entityManager->getRepository(Post::class)->findAll();
+
+        echo "<pre>";
+        //var_dump($posts);
+        echo "</pre>";
+
         return new Response('Hello Johny! :-)');
     }
 }
